857921e9a74a0bcf52cdaf3b2fff0d528380ae3b
Dipeshkumar Joshi <148765378+dipeshjoshi292@users.noreply.github.com>
2025-03-16 22:37:56 +0530

    python lec2.py

--- python lec2.py
+++ python lec2.py
@@ -1,5 +1,5 @@
 #LIST
-'''
+
 a=[1,'banana','python',2]
 print(type(a))
 print(a)
@@ -7,9 +7,9 @@ print(a[3:])
 print(a[0:2])
 print(a+a)
 print(a*3)
-'''
+
 #TUPLES
-'''
+
 tup=(1,"python",2)
 print(type(tup))
 print(tup)
@@ -17,41 +17,41 @@ print(tup[1:])
 print(tup+tup)
 print(tup*3)
 print(tup[-1])
-'''
-'''
+
+
 a=(2)
 print(type(a))
 b=(2,)
 print(type(b))
-'''
+
 #SET
-'''
+
 s={"apple","banana",True,1,False,0,2} #True represent  1 and false reresent 0
 print(s)
 s.add(5)
 print(s)
 s.remove(5)
 print(s)
-'''
+
 #control struc, modules,functions revise them
 #membership (in and not in)
-'''
+
 s={"apple","banana"}
 print("banana" in s)
 print("banana" not in s)
-'''
+
 #dictionary
-'''
+
 d={"subject" :"python",2:"data"}
 print(d)
-'''
-'''
+
+
 print(int(True))
 print(bool(3))
 print(bool(0))
-'''
+
 #operators:-
-'''
+
 "ARITHEMATIC"
 a=int(input())
 b=int(input())
@@ -63,8 +63,8 @@ rem=a%b
 floor=a//b
 exp=a**b
 print(add,"\n",sub,"\n",mul,"\n",div,"\n",rem,"\n",floor,"\n",exp)
-'''
-'''
+
+
 "COMPARISON"
 a=int(input())
 b=int(input())
@@ -85,7 +85,7 @@ a/=b
 print(a)
 a%=b
 print(a)
-'''
+
 
 
 
