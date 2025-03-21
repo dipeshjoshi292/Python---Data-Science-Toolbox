1f4f73ac961af24de5caa61a3d09ee66040d673b
Dipeshkumar Joshi <148765378+dipeshjoshi292@users.noreply.github.com>
2025-03-21 19:53:13 +0530

    Update Python lec-04 - Numpy.py

--- Python lec-04 - Numpy.py
+++ Python lec-04 - Numpy.py
@@ -7,12 +7,12 @@ Created in 2005.
 Array  object in Numpy is  called ndarray.(n dimensional array)
 In Numpy dimensions are called axes. The number of axes is rank.
 '''
-'''
+
 #Creating Numpy array from Python list/tuples
 import numpy as np
 a=np.array([1,2,3,4])
 print(a)
-'''
+
 n=int (input())
 if(n%2==0):
     print("even")
