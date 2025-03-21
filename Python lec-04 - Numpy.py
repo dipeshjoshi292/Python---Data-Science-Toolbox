ce7bfc2d4c51473fc5997d50c2535982e2a1cf1e
Dipeshkumar Joshi <148765378+dipeshjoshi292@users.noreply.github.com>
2025-03-21 19:51:16 +0530

    Add files via upload

---
+++ Python lec-04 - Numpy.py
@@ -0,0 +1,20 @@
+#Python Libraries for Machine Learning
+#Numpy
+'''
+Numerical Python
+provide fast mathematical function.Used for working with arrays.
+Created in 2005.
+Array  object in Numpy is  called ndarray.(n dimensional array)
+In Numpy dimensions are called axes. The number of axes is rank.
+'''
+'''
+#Creating Numpy array from Python list/tuples
+import numpy as np
+a=np.array([1,2,3,4])
+print(a)
+'''
+n=int (input())
+if(n%2==0):
+    print("even")
+else:
+    print("odd")
